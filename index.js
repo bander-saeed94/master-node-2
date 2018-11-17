83e7441acfdba09aaac4154683db69eea9298ff3
Bander Alshammari <bannadr1@hotmail.com>
2018-11-17 23:27:57 +0300

    workers added

---
+++ index.js
@@ -0,0 +1,19 @@
+/*
+ * Primary file for API
+ *
+ */
+
+const server = require('./lib/server')
+const workers = require('./lib/workers')
+
+const app = {}
+
+app.init = () => {
+    server.init()
+    workers.init()
+}
+
+
+app.init()
+
+module.exports = app
